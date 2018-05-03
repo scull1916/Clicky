503bda727909c2c12e12c82cf0954b9ec36225d7
scull1916 <kpscully@gmail.com>
2018-05-03 13:21:50 -0400

    still working on click but deployable to heroku

--- src/components/gameboard/gameboard.js
+++ src/components/gameboard/gameboard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Gameboard.css';
 
 const Gameboard = props => (
-	<div onClick={() => props.click(props.id)} className = "card">
+	<div id={props.id} onClick={props.imageClick} className = "card">
 		<div className = "img-container">
 			<img alt={props.name} src={props.image} />
 		</div>
